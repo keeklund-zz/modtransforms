9008f03484a75de48f043faf73f86548b7d1f281
Karl Eklund <keklund@email.unc.edu>
2015-04-13 13:47:13 -0400

    separation not necessary

--- modtransforms/run/arguments.py
+++
@@ -1,8 +0,0 @@
-from os import path
-from argparse import ArgumentParser
-
-def parse_arguments():
-    parser = ArgumentParser(prog=path.splitext(path.basename(__file__))[0],
-                            description='Update positions using MOD File')
-    parser.add_argument('--mod', required=True, help='Required MOD File')
-    args = parser.parse_args()
