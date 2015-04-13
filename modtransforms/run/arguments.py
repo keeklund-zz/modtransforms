f763125f117465fec96710c4e952b4f7b9e6cd4b
Karl Eklund <keklund@email.unc.edu>
2015-04-13 13:44:02 -0400

    still initializing

--- modtransforms/run/arguments.py
+++ modtransforms/run/arguments.py
@@ -1,6 +1,7 @@
+from os import path
 from argparse import ArgumentParser
 
-def parse_argument():
+def parse_arguments():
     parser = ArgumentParser(prog=path.splitext(path.basename(__file__))[0],
                             description='Update positions using MOD File')
     parser.add_argument('--mod', required=True, help='Required MOD File')
