328edc999ea633dd04eb627e51c0825fcf320152
Karl Eklund <keklund@email.unc.edu>
2015-04-13 12:05:31 -0400

    separated transform from IO read

---
+++ modtransforms/mod/stream.py
@@ -0,0 +1,7 @@
+def gen_mod(mod_file):
+    """Return mod file generator"""
+    with gzip.open(mod_file, 'r') as mod:
+        for line in mod:
+            if not line.startswith('#'):
+                yield line.rstrip()
+
