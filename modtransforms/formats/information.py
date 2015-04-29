15d6ebfdd1528de53bec8e1feb4c1cb329a1140c
Karl Eklund <keklund@ad.unc.edu>
2015-04-29 14:17:05 -0400

    new methods

--- modtransforms/formats/information.py
+++ modtransforms/formats/information.py
@@ -1,9 +1,15 @@
-from modtransforms.formats.process import bam, gtf, smrna_bed, smrna_gff3
+from modtransforms.formats.process import bam, gtf, smrna_12_bed, smrna_bed
+from modtransforms.formats.process import smrna_gff3, smrna_lib_fa
+from modtransforms.formats.process import smrna_table_txt, smrna_txt
 
 file_types = {
     'bam': bam,
     'gtf': gtf,
+    'smrna_12_bed': smrna_12_bed,
     'smrna_bed': smrna_bed,
     'smrna_gff3': smrna_gff3,
+    'smrna_lib_fa': smrna_lib_fa,
+    'smrna_table_txt': smrna_table_txt,
+    'smrna_txt': smrna_txt,
 }
 
