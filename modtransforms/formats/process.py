7652da1426d71a74f3dd1e794d2294b5c450496a
Karl Eklund <keklund@ad.unc.edu>
2015-04-29 14:16:47 -0400

    quick hacks for multiple file formats. refactor soon

--- modtransforms/formats/process.py
+++ modtransforms/formats/process.py
@@ -1,87 +1,6 @@
+from re import match
 from pysam import AlignmentFile
 from modtransforms.mod.transform import build_transform, find_delta
-
-def gtf(args, logger):
-    out = open(args.output, 'w')
-    chrom_mods = build_transform(args.mod, logger, args.reverse)
-    keys = 'contig feature source start end score strand frame attributes'
-    curr_chrom = ""
-    with open(args.input, 'r') as input_:
-        for line in input_:
-            gene = line.rstrip()
-            data = {k:g for k,g in zip(keys.split(), gene.split('\t'))}
-            if data.get('contig') != curr_chrom:
-                curr_chrom = data.get('contig')
-                try:
-                    positions, deltas = zip(*chrom_mods.get(curr_chrom))
-                    logger.info("CONTIG: '%s'" % curr_chrom)
-                except TypeError:
-                    logger.warn(
-                        "CONTIG: '%s' is not in MOD File. Skipping." % \
-                        curr_chrom)
-                    positions, deltas = [], 0
-                    continue
-            try:
-                start_delta = find_delta(positions, 
-                                         deltas, 
-                                         int(data.get('start')))
-                end_delta = find_delta(positions, 
-                                       deltas,
-                                       int(data.get('end')))
-                data['start'] = int(data.get('start')) + start_delta
-                data['end'] = int(data.get('end')) + end_delta
-                out.write('%s\n' % \
-                          '\t'.join([str(data.get(k)) for k in keys.split()]))
-            except IndexError:
-                pass
-            
-def smrna_gff3(args, logger):
-    out = open(args.output, 'w')
-    chrom_mods = build_transform(args.mod, logger, args.reverse)
-    keys = 'chrom chromStart chromEnd name score strand'
-    curr_chrom = ""
-    with open(args.input, 'r') as input_:
-        for line in input_:
-            if not line.startswith('#'):
-                gene = line.rstrip()
-                data = {k:g for k,g in zip(keys.split(), gene.split('\t'))}
-                if data.get('chrom') != curr_chrom:
-                    curr_chrom = data.get('chrom')
-                    positions, deltas = zip(*chrom_mods.get(curr_chrom))
-                start_delta = find_delta(positions, 
-                                         deltas, 
-                                         int(data.get('chromStart')))
-                end_delta = find_delta(positions, 
-                                       deltas,
-                                       int(data.get('chromEnd')))
-                data['chromStart'] = int(data.get('chromStart')) + start_delta
-                data['chromEnd'] = int(data.get('chromEnd')) + end_delta
-                out.write('%s\n' % \
-                              '\t'.join([str(data.get(k)) for k in keys.split()]))
-
-def smrna_bed(args, logger):
-
-    out = open(args.output, 'w')
-    chrom_mods = build_transform(args.mod, logger, args.reverse)
-    keys = 'chrom chromStart chromEnd name score strand'
-    curr_chrom = ""
-    with open(args.input, 'r') as input_:
-        for line in input_:
-            gene = line.rstrip()
-            data = {k:g for k,g in zip(keys.split(), gene.split('\t'))}
-            if data.get('chrom') != curr_chrom:
-                curr_chrom = data.get('chrom')
-                positions, deltas = zip(*chrom_mods.get(curr_chrom))
-            start_delta = find_delta(positions, 
-                                     deltas, 
-                                     int(data.get('chromStart')))
-            end_delta = find_delta(positions, 
-                                   deltas,
-                                   int(data.get('chromEnd')))
-            data['chromStart'] = int(data.get('chromStart')) + start_delta
-            data['chromEnd'] = int(data.get('chromEnd')) + end_delta
-            out.write('%s\n' % \
-                      '\t'.join([str(data.get(k)) for k in keys.split()]))
 
 def bam(args, logger):
     chrom_mods = build_transform(args.mod, logger, args.reverse)
@@ -138,3 +57,270 @@ def bam(args, logger):
             output.write(line)
         except IndexError:
             pass
+
+def gtf(args, logger):
+    out = open(args.output, 'w')
+    chrom_mods = build_transform(args.mod, logger, args.reverse)
+    keys = 'contig feature source start end score strand frame attributes'
+    curr_chrom = ""
+    with open(args.input, 'r') as input_:
+        for line in input_:
+            gene = line.rstrip()
+            data = {k:g for k,g in zip(keys.split(), gene.split('\t'))}
+            if data.get('contig') != curr_chrom:
+                curr_chrom = data.get('contig')
+                try:
+                    positions, deltas = zip(*chrom_mods.get(curr_chrom))
+                    logger.info("CONTIG: '%s'" % curr_chrom)
+                except TypeError:
+                    logger.warn(
+                        "CONTIG: '%s' is not in MOD File. Skipping." % \
+                        curr_chrom)
+                    positions, deltas = [], 0
+                    continue
+            try:
+                start_delta = find_delta(positions, 
+                                         deltas, 
+                                         int(data.get('start')))
+                end_delta = find_delta(positions, 
+                                       deltas,
+                                       int(data.get('end')))
+                data['start'] = int(data.get('start')) + start_delta
+                data['end'] = int(data.get('end')) + end_delta
+                out.write('%s\n' % \
+                          '\t'.join([str(data.get(k)) for k in keys.split()]))
+            except IndexError:
+                pass
+            
+def smrna_12_bed(args, logger):
+
+    out = open(args.output, 'w')
+    chrom_mods = build_transform(args.mod, logger, args.reverse)
+    keys = 'chrom chromStart1 chromEnd1 name score strand chromStart2 chromEnd2 0,0,0 1 0 chromStart3'
+    curr_chrom = ""
+    with open(args.input, 'r') as input_:
+        for line in input_:
+            if not line.startswith('chrom'):
+                gene = line.rstrip()
+                data = {k:g for k,g in zip(keys.split(), gene.split('\t'))}
+                if data.get('chrom') != curr_chrom:
+                    curr_chrom = data.get('chrom')
+                    try:
+                        positions, deltas = zip(*chrom_mods.get(curr_chrom))
+                        logger.info("CONTIG: '%s'" % curr_chrom)
+                    except TypeError:
+                        logger.warn(
+                            "CONTIG: '%s' is not in MOD File. Skipping." % \
+                            curr_chrom)
+                        positions, deltas = [], 0
+                        continue
+                try:
+                    start_delta = find_delta(positions, 
+                                             deltas, 
+                                             int(data.get('chromStart1')))
+                    end_delta = find_delta(positions, 
+                                           deltas,
+                                           int(data.get('chromEnd1')))
+                    start_delta = find_delta(positions, 
+                                             deltas, 
+                                             int(data.get('chromStart2')))
+                    end_delta = find_delta(positions, 
+                                           deltas,
+                                           int(data.get('chromEnd2')))
+                    start_delta = find_delta(positions, 
+                                             deltas, 
+                                             int(data.get('chromStart3')))
+                    data['chromStart1'] = int(data.get('chromStart1')) + start_delta
+                    data['chromEnd1'] = int(data.get('chromEnd1')) + end_delta
+                    data['chromStart2'] = int(data.get('chromStart2')) + start_delta
+                    data['chromEnd2'] = int(data.get('chromEnd2')) + end_delta
+                    try:
+                        data['chromStart3'] = int(data.get('chromStart3')) + start_delta
+                    except:
+                        pass
+                    out.write('%s\n' % \
+                              '\t'.join([str(data.get(k)) for k in keys.split()]))
+                except IndexError:
+                    pass
+
+def smrna_bed(args, logger):
+
+    out = open(args.output, 'w')
+    chrom_mods = build_transform(args.mod, logger, args.reverse)
+    keys = 'chrom chromStart chromEnd name score strand'
+    curr_chrom = ""
+    with open(args.input, 'r') as input_:
+        for line in input_:
+            if not line.startswith('chrom'):
+                gene = line.rstrip()
+                data = {k:g for k,g in zip(keys.split(), gene.split('\t'))}
+                if data.get('chrom') != curr_chrom:
+                    curr_chrom = data.get('chrom')
+                    try:
+                        positions, deltas = zip(*chrom_mods.get(curr_chrom))
+                        logger.info("CONTIG: '%s'" % curr_chrom)
+                    except TypeError:
+                        logger.warn(
+                            "CONTIG: '%s' is not in MOD File. Skipping." % \
+                            curr_chrom)
+                        positions, deltas = [], 0
+                        continue
+                try:
+                    start_delta = find_delta(positions, 
+                                             deltas, 
+                                             int(data.get('chromStart')))
+                    end_delta = find_delta(positions, 
+                                           deltas,
+                                           int(data.get('chromEnd')))
+                    data['chromStart'] = int(data.get('chromStart')) + start_delta
+                    data['chromEnd'] = int(data.get('chromEnd')) + end_delta
+                    out.write('%s\n' % \
+                              '\t'.join([str(data.get(k)) for k in keys.split()]))
+                except IndexError:
+                    pass
+            
+def smrna_gff3(args, logger):
+    out = open(args.output, 'w')
+    chrom_mods = build_transform(args.mod, logger, args.reverse)
+    keys = 'chrom source type start end score strand phase attributes'
+    curr_chrom = ""
+    with open(args.input, 'r') as input_:
+        for line in input_:
+            if not line.startswith('#'):
+                gene = line.rstrip()
+                data = {k:g for k,g in zip(keys.split(), gene.split('\t'))}
+                if data.get('chrom') != curr_chrom:
+                    curr_chrom = data.get('chrom')
+                    try:
+                        positions, deltas = zip(*chrom_mods.get(curr_chrom))
+                        logger.info("CONTIG: '%s'" % curr_chrom)
+                    except TypeError:
+                        logger.warn(
+                            "CONTIG: '%s' is not in MOD File. Skipping." % \
+                            curr_chrom)
+                        positions, deltas = [], 0
+                        continue
+                try:
+                    start_delta = find_delta(positions, 
+                                             deltas, 
+                                             int(data.get('start')))
+                    end_delta = find_delta(positions, 
+                                           deltas,
+                                           int(data.get('end')))
+                    data['start'] = int(data.get('start')) + start_delta
+                    data['end'] = int(data.get('end')) + end_delta
+                    out.write('%s\n' % \
+                              '\t'.join([str(data.get(k)) for k in keys.split()]))
+                except IndexError:
+                    pass
+                
+def smrna_lib_fa(args, logger):
+
+    out = open(args.output, 'w')
+    chrom_mods = build_transform(args.mod, logger, args.reverse)
+    keys = 'chrom start end strand'
+    curr_chrom = ""
+    with open(args.input, 'r') as input_:
+        for line in input_:
+            if line.startswith('>'):
+                gene = line.rstrip()
+
+                gene_list = match('>(.*):(\d+)-(\d+)\((.*)\)', gene)
+                data = {k:g for k,g in zip(keys.split(), gene_list.groups())}
+                if data.get('chrom') != curr_chrom:
+                    curr_chrom = data.get('chrom')
+                    try:
+                        positions, deltas = zip(*chrom_mods.get(curr_chrom))
+                        logger.info("CONTIG: '%s'" % curr_chrom)
+                    except TypeError:
+                        logger.warn(
+                            "CONTIG: '%s' is not in MOD File. Skipping." % \
+                            curr_chrom)
+                        positions, deltas = [], 0
+                        continue
+                try:
+                    start_delta = find_delta(positions, 
+                                             deltas, 
+                                             int(data.get('start')))
+                    end_delta = find_delta(positions, 
+                                           deltas,
+                                           int(data.get('end')))
+                    data['start'] = int(data.get('start')) + start_delta
+                    data['end'] = int(data.get('end')) + end_delta
+                    out.write('>%s:%s-%s(%s)\n' % \
+                              tuple([str(data.get(k)) for k in keys.split()]))
+                except IndexError:
+                    pass
+            else:
+                out.write('%s' % line)
+                
+def smrna_table_txt(args, logger):
+    
+    out = open(args.output, 'w')
+    chrom_mods = build_transform(args.mod, logger, args.reverse)
+    keys = 'name chrom start end strand mature hairpin'
+    curr_chrom = ""
+    with open(args.input, 'r') as input_:
+        for line in input_:
+            gene = line.rstrip()
+            data = {k:g for k,g in zip(keys.split(), gene.split())}
+            if data.get('chrom') != curr_chrom:
+                curr_chrom = data.get('chrom')
+                try:
+                    positions, deltas = zip(*chrom_mods.get(curr_chrom))
+                    logger.info("CONTIG: '%s'" % curr_chrom)
+                except TypeError:
+                    logger.warn(
+                        "CONTIG: '%s' is not in MOD File. Skipping." % \
+                        curr_chrom)
+                    positions, deltas = [], 0
+                    continue
+            try:
+                start_delta = find_delta(positions, 
+                                         deltas, 
+                                         int(data.get('start')))
+                end_delta = find_delta(positions, 
+                                       deltas,
+                                       int(data.get('end')))
+                data['start'] = int(data.get('start')) + start_delta
+                data['end'] = int(data.get('end')) + end_delta
+                out.write('%s\n' % \
+                          '\t'.join([str(data.get(k)) for k in keys.split()]))
+            except IndexError:
+                pass
+
+def smrna_txt(args, logger):
+
+    out = open(args.output, 'w')
+    chrom_mods = build_transform(args.mod, logger, args.reverse)
+    keys = 'name num_trna trna_begin trna_end isotype anticodon up_region down_region intron_begin intron_end cove hmm 2_str scanner'
+    curr_chrom = ""
+    with open(args.input, 'r') as input_:
+        for line in input_:
+            gene = line.rstrip()
+            data = {k:g for k,g in zip(keys.split(), gene.split())}
+            if data.get('name') != curr_chrom:
+                curr_chrom = data.get('name')
+                try:
+                    positions, deltas = zip(*chrom_mods.get(curr_chrom))
+                    logger.info("CONTIG: '%s'" % curr_chrom)
+                except TypeError:
+                    logger.warn(
+                        "CONTIG: '%s' is not in MOD File. Skipping." % \
+                        curr_chrom)
+                    positions, deltas = [], 0
+                    continue
+            try:
+                start_delta = find_delta(positions, 
+                                         deltas, 
+                                         int(data.get('trna_begin')))
+                end_delta = find_delta(positions, 
+                                       deltas,
+                                       int(data.get('trna_end')))
+                data['trna_begin'] = int(data.get('trna_begin')) + start_delta
+                data['trna_end'] = int(data.get('trna_end')) + end_delta
+                out.write('%s\n' % \
+                          '\t'.join([str(data.get(k)) for k in keys.split()]))
+            except IndexError:
+                pass
+
