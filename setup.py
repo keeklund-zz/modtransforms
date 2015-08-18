49483ee9a71f2851a30d7ea49a189f98d08f810a
Karl Eklund <keklund@ad.unc.edu>
2015-08-18 16:09:17 -0400

    atac ready

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 """ """
 from setuptools import setup, find_packages
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 # setup packages to be installed
 packages = find_packages(exclude=['*.tests',])
